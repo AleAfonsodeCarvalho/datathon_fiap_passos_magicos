0ccbe98727f5afc1cfd172d1f201b8aeb5fca03c
Alexandre Afonso de Carvalho <143330426+AleAfonsodeCarvalho@users.noreply.github.com>
2026-03-08 03:03:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -172,9 +172,13 @@ with col_f1:
     st.markdown("###Transformando o Brasil através da Educação")
     st.write("A Passos Mágicos transforma vidas através da educação e protagonismo. Este projeto visa apoiar essa missão com dados.")
 
-    st.write("**Você sabia?** O programa da Passos Mágicos inclui educação de qualidade, suporte psicopedagógico e 
-                atividades de protagonismo. Este modelo preditivo é uma ferramenta de apoio para que 
-                essa 'fórmula mágica' chegue a quem mais precisa no momento certo."")
+ st.info("""
+    **Você sabia?** O programa da Passos Mágicos inclui educação de qualidade, suporte psicopedagógico e 
+    atividades de protagonismo. Este modelo preditivo é uma ferramenta de apoio para que 
+    essa 'fórmula mágica' chegue a quem mais precisa no momento certo.
+    
+    [Visite o site da Associação](https://passosmagicos.org.br/)
+    """)
     
     st.link_button("Conheça o site oficial", "https://passosmagicos.org.br/")
 
