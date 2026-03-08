62fe096c406354e95557c6b0dee93b506407d3b5
Alexandre Afonso de Carvalho <143330426+AleAfonsodeCarvalho@users.noreply.github.com>
2026-03-08 04:33:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ st.set_page_config(page_title="Passos Mágicos - Mentor Digital", layout="wide",
 # CSS personalizado
 st.markdown("""
     <style>
-        .stApp { background-color: #5799CA; color: #1E3A8A; }
-        h1, h2, h3 { color: #1E3A8A !important; font-weight: 700; }
+        .stApp { background-color: #F2F2F2; color: #1E3A8A; }
+        h1, h2, h3 { color: #1E3A8A !important; font-weight: 800; }
         div.stButton > button:first-child {
             background-color: #FFC107; color: #1E3A8A; border-radius: 10px; border: none;
             font-weight: bold; height: 3em; transition: 0.3s;
